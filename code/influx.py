93154f290d2324ae8b0634c4bd6a7753acb41593
Vikky Omkar <vikkyomkar@gmail.com>
2020-04-12 20:51:23 +0530

    comment console log

--- code/influx.py
+++ code/influx.py
@@ -3,6 +3,6 @@ import subprocess
 def Post(db,postQuery):
 	influxPost = "curl -i -XPOST 'http://localhost:8086/write?db={0}' --data-binary ".format(db)
 	postData = '{0} "{1}"'.format(influxPost,postQuery)
-	print(postData)
+	#print(postData)
 	ps = subprocess.Popen(postData,shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	print(ps.communicate()[0])
+	#print(ps.communicate()[0])
