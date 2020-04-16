783b27906c7cf670f649481f4f5f0ca9749816ef
Vikky Omkar <vikkyomkar@gmail.com>
2020-04-16 18:23:28 +0530

    updated url endpoint

--- code/recoveredDeceased.py
+++ code/recoveredDeceased.py
@@ -6,7 +6,7 @@ import json
 import influx
 
 ### Variables ####
-url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSc_2y5N0I67wDU38DjDh35IZSIS30rQf7_NYZhtYYGU1jJYT6_kDx4YpF-qw0LSlGsBYP8pqM_a1Pd/pub?output=csv&gid=200733542"
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSz8Qs1gE_IYpzlkFkCXGcL_BqR8hZieWVi-rphN1gfrO3H4lDtVZs4kd0C3P8Y9lhsT1rhoB-Q_cP4/pub?output=csv&gid=200733542"
 db = sys.argv[1]
 table = 'covid_india'
 
