d484475ea9ab0aa42f14cc9362ef1c250b92b592
Vikky Omkar <vikkyomkar@gmail.com>
2020-04-22 09:12:27 +0530

    Update infectioncause.py

--- code/infectioncause.py
+++ code/infectioncause.py
@@ -8,70 +8,65 @@ import influx
 db = sys.argv[1] 
 table = "covid_india"
 
-with open('/mnt/covid/rawdata') as f:
+with open('/mnt/covid/code/rawdata') as f:
 	data = json.load(f)
 
-medicalStaff = ['doctor','compounder','nurse','health worker','private hospital','hospital staff']
-familyTransmission = ['family','husband','wife','daughter','father','mother','siblings','son','spouse','close contact']
-localTransmission = [ 'relative','neighbour','no travel history','friend','teacher','car','contact transmission','local transmission','domestic worker','no history of travel','locally transmitted','tenant','contact','work','passenger','cab driver']
-travelAbroad = ['wuhan','austria','italy','Dubai','iran','thailand','malaysia','oman','foreign','us','london','japan','greece','saudi','qatar','france','philippines','spain','russia','netherlands','middle east','uae','united kingdom','indonesia','turkey','germany','dublin','ireland','mexico','finland','portugal','scotland','singapore','sweden','denmark','abroad','baharain','australia']
+MedicalStaff = ['doctor','compounder','nurse','health worker','private hospital','hospital staff']
+FamilyTransmission = ['family','husband','wife','daughter','father','mother','siblings','son','spouse','close contact']
+LocalTransmission = [ 'relative','neighbour','no travel history','friend','teacher','car','contact transmission','local transmission','domestic worker','no history of travel','locally transmitted','tenant','contact','work','passenger','cab driver']
+TravelledAbroad = ['wuhan','austria','italy','Dubai','iran','thailand','malaysia','oman','foreign','us','london','japan','greece','saudi','qatar','france','philippines','spain','russia','netherlands','middle east','uae','united kingdom','indonesia','turkey','germany','dublin','ireland','mexico','finland','portugal','scotland','singapore','sweden','denmark','abroad','baharain','australia']
 
-causeDict = {'medicalStaff' : 0, 'familyTransmission':0, 'localTransmission':0,'travelAbroad':0,'delhitravel':0,'domestictravel':0}
+causeDict = {'MedicalStaff' : 0, 'FamilyTransmission':0, 'LocalTransmission':0,'TravelledAbroad':0,'TravelToDelhi':0,'DomesticTravel':0}
 
 for patient in data['raw_data']:
 	try:
 		flag = 0
 		if patient['notes'] != "":
 			data=patient['notes'].lower()
-			for ele in medicalStaff:
+			for ele in MedicalStaff:
 				if ele in data:
 					if flag != 1:
-						causeDict['medicalStaff'] += 1
+						causeDict['MedicalStaff'] += 1
 						flag = 1
 
 			if flag != 1:	
-				for ele in familyTransmission:
+				for ele in FamilyTransmission:
 					if ele in data:
 						if flag != 1:
-							causeDict['familyTransmission'] += 1
+							causeDict['FamilyTransmission'] += 1
 							flag = 1
 			if flag != 1:
-				for ele in localTransmission:
+				for ele in LocalTransmission:
 					if ele in data:
 						if flag != 1:
-							causeDict['localTransmission'] += 1
+							causeDict['LocalTransmission'] += 1
 							flag = 1
 			"""
 			if flag != 1:
-				for ele in localTransmission:
+				for ele in LocalTransmission:
                                 	if ele in data:
                                         	if flag != 1:
-							causeDict['localTransmission'] += 1
+							causeDict['LocalTransmission'] += 1
 							flag = 1
 			"""
 			flag = 0
-			for ele in travelAbroad:
+			for ele in TravelledAbroad:
 				if 'travel' in data and ele in data:
 					if flag != 1:
-						causeDict['travelAbroad'] += 1
+						causeDict['TravelledAbroad'] += 1
 						flag = 1
 						
 			if 'travel' in data and 'delhi' in data:
 				if flag != 1:
-					causeDict['delhitravel'] += 1
+					causeDict['TravelToDelhi'] += 1
 					flag = 1
 			
 			if 'travel' in data or 'visit' in data:
 				if flag != 1:
-					causeDict['domestictravel'] += 1
+					causeDict['DomesticTravel'] += 1
 	except Exception as e:
 		print(e)
 
 for k,v in causeDict.items():
 	postQuery="{0},infectionType={1} totalinfections={2}".format(table,k,v)
-	influx.Post(db,postQuery)	
-
-
-
-
-
+	influx.Post(db,postQuery)
