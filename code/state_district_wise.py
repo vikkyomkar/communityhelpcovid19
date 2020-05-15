145718bf77c6f5640e6a747bb93e2f2c16a6935f
Vikky Omkar <vikkyomkar@gmail.com>
2020-05-15 15:27:39 +0530

    Update state_district_wise.py

--- code/state_district_wise.py
+++ code/state_district_wise.py
@@ -9,38 +9,46 @@ import influx
 db=sys.argv[1]
 table = 'covid_india'
 
-def state_district_wise(datajson):
-	state_dict = {'Jammu_and_Kashmir' : 'Jammu_Kashmir', 'Dadra_and_Nagar_Haveli' : 'Dadra_Nagar_Haveli', 'Andaman_and_Nicobar_Islands' : 'Andaman_Nicobar'}
+def state_district_wise(datajson,zonejson):
+
+	color = {'Green' : 1, 'Orange' : 3, 'Red' : 5}
+	zoneDict = {}
+	for key in zonejson['zones']:
+		if key['zone'] != '':
+			zoneDict[key['district']] = key['zone']
 	for key in datajson.keys():
 		try:
 			for key2 in datajson[key]['districtData']:
-				statename = '_'.join(key.split(' '))   
-				if statename in state_dict.keys():
-                                        state = state_dict[statename]
-                                else:
-                                        state = statename
+				if key2 in zoneDict.keys():
+					zonecode = color[zoneDict[key2]]
+				else:
+					zonecode = 7
 
+				state = '\ '.join(key.strip().split(' '))
 				district = '_'.join(key2.split(' '))
 		
-				active = datajson[key]['districtData'][key2]['active']
-				confirmed = datajson[key]['districtData'][key2]['confirmed']
-				deceased = datajson[key]['districtData'][key2]['deceased']
-				recovered = datajson[key]['districtData'][key2]['recovered']
-				deltaconfirmed = datajson[key]['districtData'][key2]['delta']['confirmed']
-				deltadeaths = datajson[key]['districtData'][key2]['delta']['deceased']
-				deltarecovered =  datajson[key]['districtData'][key2]['delta']['recovered']
-
-				postQuery = "{0},state={1},district={2} dactive={3},dconfirmed={4},ddeaths={5},drecovered={6},ddeltaconfirmed={7},ddeltadeaths={8},ddeltarecovered={9}".format(table,state,district,active,confirmed,deceased,recovered,deltaconfirmed,deltadeaths,deltarecovered)
-				
+				active = abs(datajson[key]['districtData'][key2]['active'])
+				confirmed = abs(datajson[key]['districtData'][key2]['confirmed'])
+				deceased = abs(datajson[key]['districtData'][key2]['deceased'])
+				recovered = abs(datajson[key]['districtData'][key2]['recovered'])
+				deltaconfirmed = abs(datajson[key]['districtData'][key2]['delta']['confirmed'])
+				deltadeaths = abs(datajson[key]['districtData'][key2]['delta']['deceased'])
+				deltarecovered =  abs(datajson[key]['districtData'][key2]['delta']['recovered'])
+				postQuery = '{0},state={1},district={2} district=\"{3}\",dactive={4},dconfirmed={5},ddeaths={6},drecovered={7},ddeltaconfirmed={8},ddeltadeaths={9},ddeltarecovered={10},zone={11}'.format(table,state,district,key2,active,confirmed,deceased,recovered,deltaconfirmed,deltadeaths,deltarecovered,zonecode)
+				#print(postQuery)	
 				influx.Post(db,postQuery)
 		except Exception as e:		
 			print(e)
 			sys.exit(1)
 def main():
 	try:
-		with open('/mnt/covid/code/state_district_wise') as f:
-			datajson = json.load(f)
-			state_district_wise(datajson)
+		with open('/mnt/covid/code/districtZone') as f1:
+			zonejson = json.load(f1)
+
+		with open('/mnt/covid/code/state_district_wise') as f2:
+			datajson = json.load(f2)
+
+		state_district_wise(datajson,zonejson)
 	except Exception as e:
 		print(e)
 		sys.exit(1)
