f2cad1fb8d00e00c13654b2b307fd036b6855862
Vikky Omkar <vikkyomkar@gmail.com>
2020-06-18 02:52:40 +0530

    Vikkyomkar patch 15 (#154)
    * code updated
    
    * Update state_district_wise.py

--- code/state_district_wise.py
+++ code/state_district_wise.py
@@ -16,6 +16,15 @@ def state_district_wise(datajson,zonejson):
 	for key in zonejson['zones']:
 		if key['zone'] != '':
 			zoneDict[key['district']] = key['zone']
+
+	delhi_dactive = 0
+	delhi_dconfirmed = 0
+	delhi_ddeaths = 0
+	delhi_drecovered = 0
+	delhi_ddeltaconfirmed = 0
+	delhi_ddeltadeaths = 0
+	delhi_ddeltarecovered = 0
+
 	for key in datajson.keys():
 		try:
 			for key2 in datajson[key]['districtData']:
@@ -34,12 +43,27 @@ def state_district_wise(datajson,zonejson):
 				deltaconfirmed = abs(datajson[key]['districtData'][key2]['delta']['confirmed'])
 				deltadeaths = abs(datajson[key]['districtData'][key2]['delta']['deceased'])
 				deltarecovered =  abs(datajson[key]['districtData'][key2]['delta']['recovered'])
-				postQuery = '{0},state={1},district={2} district=\"{3}\",dactive={4},dconfirmed={5},ddeaths={6},drecovered={7},ddeltaconfirmed={8},ddeltadeaths={9},ddeltarecovered={10},zone={11}'.format(table,state,district,key2,active,confirmed,deceased,recovered,deltaconfirmed,deltadeaths,deltarecovered,zonecode)
-				#print(postQuery)	
-				influx.Post(db,postQuery)
+
+
+				if state ==  'Delhi':
+					delhi_dactive += active
+					delhi_dconfirmed += confirmed
+					delhi_ddeaths += deceased
+					delhi_drecovered += recovered
+					delhi_ddeltaconfirmed += deltaconfirmed
+					delhi_ddeltadeaths += deltadeaths 
+					delhi_ddeltarecovered += deltarecovered
+				else:
+					postQuery = '{0},state={1},district={2} district=\"{3}\",dactive={4},dconfirmed={5},ddeaths={6},drecovered={7},ddeltaconfirmed={8},ddeltadeaths={9},ddeltarecovered={10},zone={11}'.format(table,state,district,key2,active,confirmed,deceased,recovered,deltaconfirmed,deltadeaths,deltarecovered,zonecode)
+					#print(postQuery)	
+					influx.Post(db,postQuery)
 		except Exception as e:		
 			print(e)
 			sys.exit(1)
+	postQuery = '{0},state={1},district={2} district=\"{3}\",dactive={4},dconfirmed={5},ddeaths={6},drecovered={7},ddeltaconfirmed={8},ddeltadeaths={9},ddeltarecovered={10}'.format(table,'Delhi','Delhi','Delhi',delhi_dactive,delhi_dconfirmed,delhi_ddeaths,delhi_drecovered,delhi_ddeltaconfirmed,delhi_ddeltadeaths,delhi_ddeltarecovered)
+	#print(postQuery)
+	influx.Post(db,postQuery)
+
 def main():
 	try:
 		with open('/mnt/covid/code/districtZone') as f1:
